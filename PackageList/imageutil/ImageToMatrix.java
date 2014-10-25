5eee8e77018485bb1280d05237bde38d98843921
EnricoFabris <enricofabris93@gmail.com>
2014-10-25 15:25:42 +0200

    INSERITE INFO VARIE E PRIME UTILITY A SCOPO GENERALE
    HO INSERITO ALCUNI FILE DI TESTO CONTENENTI LE LINEE GUIDA DA SEGUIRE
    E LE METODOLOGIE DA USARE PER SCRIVERE IL CODICE.
    SIETE PREGATI DI SEGUIRLE.
    HO INIZIATO A CARICARE ANCHE LE UTILITY PER IL PRE-PROCESSING
    DELL'IMMAGINE

---
+++ PackageList/imageutil/ImageToMatrix.java
@@ -0,0 +1,28 @@
+package imageutil;
+
+import java.awt.image.BufferedImage;
+
+public class ImageToMatrix {
+    
+    private BufferedImage image;
+    private int[][] matrixImage;
+    private int width, height;
+            
+    
+    public ImageToMatrix(BufferedImage img) {
+        width = img.getWidth();
+        height = img.getHeight();
+        matrixImage = new int[width][height];
+    }
+    
+    public int[][] getMatrix() {
+        for (int h = 0; h < height; h++) {
+            for (int w = 0; w < width; w++) {
+                int color = image.getRGB(w, h);
+                matrixImage[w][h] = color;
+            }
+        } 
+        return matrixImage;
+    }
+    
+}
--- prova.java
+++
@@ -1,1 +0,0 @@
-E' solo una prova.
