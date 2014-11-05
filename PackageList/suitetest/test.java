10278124b113b8031d6a3882fd63b891d4d0d78c
KaruroChori <KaruroChori@users.noreply.github.com>
2014-11-05 22:46:45 +0100

    Update test.java

--- PackageList/suitetest/test.java
+++ PackageList/suitetest/test.java
@@ -16,9 +16,12 @@ public class test {
             GrayImage newimg = new GrayImage(tmp.getMatrix());
             GrayScaleImageEqualization equalized = new GrayScaleImageEqualization(newimg.getGrayMatrixImage());
             MedianFilter filtered = new MedianFilter(equalized.getMatrixEqualized());
+            NormalizeImage normalized= new NormalizeImage(newimg.getGrayMatrixImage());
             int dimension = 5;            
             NoiseReduction nonoise = new NoiseReduction(filtered.getFilteredMatrix(dimension));
             nonoise.getNoNoiseMatrixGrayImage();
+            normalized.ApplyFilter(1);
+            ImageIO.write(normalized.getFilteredImage(),"JPG",new File("normalized.jpg"));
             ImageIO.write(newimg.getGrayImage(),"JPG",new File("gray.jpg"));
             ImageIO.write(equalized.getImageEqualized(),"JPG",new File("equalized.jpg"));
             ImageIO.write(filtered.getFilteredImage(),"JPG",new File("filtered"+dimension+".jpg"));
